4c04881353cc02e02cdd038524819c99e4d040ca
Johan Watelet <johanwatelet@gmail.com>
2023-08-23 15:09:53 +0200

    first draw of the board

---
+++ lib/board.rb
@@ -0,0 +1,37 @@
+require_relative 'string'
+
+class Board
+  def initialize
+    @table = [
+      ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'],
+      ["\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F"],
+      [nil, nil, nil, nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil, nil, nil, nil],
+      ['♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙'],
+      ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖']
+    ]
+  end
+
+  def draw
+    puts draw_body
+  end
+
+  def draw_body
+    @table.map.with_index do |line, line_index|
+      line.map.with_index do |cell, cell_index|
+        draw_cell(cell, line_index + cell_index)
+      end.join('')
+    end.join("\n")
+  end
+
+  def draw_cell(cell, index)
+    centered_cell = cell.to_s.center(3)
+    if index.odd?
+      centered_cell.bg_black
+    else
+      centered_cell.bg_gray
+    end
+  end
+end
